8b3ea3e7684e97454af05c37bac09df57739283e
Brain Barett <brianbarett97@gmail.com>
2023-09-10 18:38:33 -0400

    [BE]feat: set up cors

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,7 +1,9 @@
 import express from 'express';
+/** @ts-ignore */
+import cors from 'cors';
 
 const app = express();
-app.use(express.json());
+app.use([express.json(), cors()]);
 
 export { default as sequelize } from '@/services/sequelize';
 
