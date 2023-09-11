921f370d34ca3f4b5d187c49bc6f44bf1ecda0b8
Brain Barett <brianbarett97@gmail.com>
2023-09-11 17:09:50 -0400

    [FE]feat: persist auth reducer

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,12 +1,15 @@
 import './app.scss';
 import type { AppProps } from 'next/app';
 import { Provider } from 'react-redux';
-import store from '@/stores';
+import store, { persistor } from '@/stores';
+import { PersistGate } from 'redux-persist/integration/react';
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<Provider store={store}>
-			<Component {...pageProps} />
+			<PersistGate loading={null} persistor={persistor}>
+				<Component {...pageProps} />
+			</PersistGate>
 		</Provider>
 	);
 }
--- client/stores/index.ts
+++ client/stores/index.ts
@@ -1,13 +1,29 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { persistReducer, persistStore } from 'redux-persist';
 import authReducer from './auth';
+import storage from 'redux-persist/lib/storage';
+import thunk from 'redux-thunk';
+import hardSet from 'redux-persist/lib/stateReconciler/hardSet';
+
+const persistConfig = {
+	key: 'root',
+	storage,
+	stateReconciler: hardSet,
+};
 
 const store = configureStore({
 	reducer: {
-		auth: authReducer,
+		auth: persistReducer<ReturnType<typeof authReducer>>(
+			persistConfig,
+			authReducer
+		),
 	},
+	devTools: process.env.NODE_ENV != 'production',
+	middleware: [thunk],
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 
 export default store;
+export const persistor = persistStore(store);
